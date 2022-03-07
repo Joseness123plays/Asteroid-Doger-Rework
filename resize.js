a5a9e573b7eff3e7c86a4b5991a86ee373ffdd19
Joseness123plays <031909pepe@gmail.com>
2022-03-06 20:40:52 -0800

    e

--- resize.js
+++ resize.js
@@ -1,10 +1,18 @@
 function Resize() {
+    let SCALE
     if(window.innerWidth<window.innerHeight){
-        document.getElementById("Title").style.fontSize = `${5/100*window.innerWidth}px`
+        SCALE = window.innerWidth
     }
     else{
-        document.getElementById("Title").style.fontSize = `${5/100*window.innerHeight}px`
+        SCALE = window.innerHeight
     }
+    document.getElementById("InnerTitle").style.fontSize = `${5/100*SCALE}px`
+    document.getElementById("InnerTitle").style.width = `${50/100*SCALE}px`
+    document.getElementById('InnerTitle').style.left = `${(window.innerWidth/2)-(50/100*SCALE/2)}px`
+    document.getElementById('InnerTitle').style.top = `${(10/100*window.innerHeight/2)-(5/100*SCALE/1.5)}px`
 }
+//1280
+//720
+//document.getElementById('Stats1').style.left = (window.innerWidth / 2) - (CanvasWidth / 2) + 'px'
 Resize()
 window.addEventListener('resize',Resize)
