fde10bf53964909fb49ff1475a4880e7fd5a2641
Joseness123plays <031909pepe@gmail.com>
2022-03-07 18:19:25 -0800

    e

--- js/resize.js
+++ js/resize.js
@@ -1,4 +1,24 @@
+const MENUS = document.getElementById('Menus')
+let Titlecolor = "red"
+const TITLECOLOR = setInterval(()=>{
+    switch (Titlecolor) {
+        case "red":
+            Titlecolor = "blue" 
+        break;
+        case "blue":
+            Titlecolor = "green" 
+        break;
+        case "green":
+            Titlecolor = "red" 
+        break;
+        default:
+            Titlecolor = "red" 
+        break;
+    }
+    document.getElementById('Title').style.borderColor = Titlecolor
+},1000)
 function Resize() {
+    const InnerTitle = document.getElementById("InnerTitle")
     let SCALE
     if(window.innerWidth<window.innerHeight){
         SCALE = window.innerWidth
@@ -6,10 +26,11 @@ function Resize() {
     else{
         SCALE = window.innerHeight
     }
-    document.getElementById("InnerTitle").style.fontSize = `${5/100*SCALE}px`
-    document.getElementById("InnerTitle").style.width = `${50/100*SCALE}px`
-    document.getElementById('InnerTitle').style.left = `${(window.innerWidth/2)-(50/100*SCALE/2)}px`
-    document.getElementById('InnerTitle').style.top = `${(10/100*window.innerHeight/2)-(5/100*SCALE/1.5)}px`
+    InnerTitle.style.fontSize = `${5/100*SCALE}px`
+    InnerTitle.style.width = `${50/100*SCALE}px`
+    InnerTitle.style.left = `${(window.innerWidth/2)-(50/100*SCALE/2)}px`
+    InnerTitle.style.top = `${(10/100*window.innerHeight/2)-(5/100*SCALE/1.5)}px`
+    document.getElementById('Title').style.borderWidth = `${1/100*SCALE}px`
 }
 //1280
 //720
